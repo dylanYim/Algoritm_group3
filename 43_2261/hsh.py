50ec59c344e54d336699e9925be8c9e727e08153
shhwang0522 <shhwangofficial@gmail.com>
2024-03-18 20:53:24 +0900

    done

--- 43_2261/hsh.py
+++ 43_2261/hsh.py
@@ -1,41 +1,42 @@
 import sys
-input = sys.stdin.readline
+n = int(sys.stdin.readline())
+
+# n개의 점을 입력 받아 2차원 리스트로 저장
+dots = []
+for _ in range(n):
+    dots.append(list(map(int, sys.stdin.readline().split())))
+
+dots.sort()  # x좌표를 기준으로 정렬
 
 
-def calc(c1, c2):
-    return (c1[0] - c2[0]) ** 2 + (c1[1] - c2[1]) ** 2
+def get_dist(a, b):  # 두 점 사이의 거리를 구하는 함수
+    return (a[0]-b[0])**2+(a[1]-b[1])**2
 
+def get_min(dots, start, end):
+    
 
-n = int(input())
-graph = sorted([tuple(map(int, input().split())) for _ in range(n)])
+    if start == end:
+        return sys.maxsize
+    elif end - start == 1:
+        return get_dist(dots[start], dots[end])
+    else:
+        mid = (start + end) //2
+        min_dist = min(get_min(dots, start, mid), get_min(dots, mid+1, end))
 
+    centers = []
+    for i in range(start, end+1): #개중요 난 range(len(dots))했음
+        if (dots[i][0]-dots[mid][0])**2 <min_dist:
+            centers.append(dots[i])
 
-def dq(left, right):
-    if left == right:
-        return float('inf')
-    elif right - left == 1:
-        return calc(graph[left], graph[right])
+    centers.sort(key=lambda x: x[1])
 
-    mid = (left + right) // 2
-    ans = min(dq(left, mid), dq(mid + 1, right))
-
-    coor = []
-    for i in range(left, right + 1):
-        d = graph[i][0] - graph[mid][0]
-        if d ** 2 < ans:
-            coor.append(graph[i])
-
-    coor.sort(key=lambda x: x[1])
-
-    for i in range(len(coor) - 1):
-        for j in range(i + 1, len(coor)):
-            d = coor[i][1] - coor[j][1]
-            if d ** 2 < ans:
-                ans = min(ans, calc(coor[i], coor[j]))
+    for i in range(len(centers)-1):
+        for j in range(i+1, len(centers)): # 이거때메 엄밀해짐
+            if (centers[i][1] -centers[j][1])**2< min_dist:
+                min_dist = min(min_dist, get_dist(centers[i], centers[j]))
             else:
                 break
+    
+    return min_dist
 
-    return ans
-
-
-print(dq(0, n - 1))
+print(get_min(dots, 0, len(dots)-1))
